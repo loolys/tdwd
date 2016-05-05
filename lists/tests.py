4de1da7da0152072b52576b2f5a690634a74fe74
loolys <pierre.liljekvist@outlook.com>
2016-05-05 20:54:47 +0200

    separated tests for can save a post and redirects after post

--- lists/tests.py
+++ lists/tests.py
@@ -10,6 +10,11 @@ from lists.views import home_page
 # Create your tests here.
 class HomePageTests(TestCase):
 
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.all().count(), 0)
+
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
@@ -29,14 +34,22 @@ class HomePageTests(TestCase):
         request.POST['item_text'] = 'A new list item'
         
         response = home_page(request)
-        self.assertIn('A new list item', response.content.decode())
-        expected_html = render_to_string(
-            'home.html',
-            {'new_item_text': 'A new list item'},
-            request=request
-        )
-        self.assertEqual(response.content.decode(), expected_html)
-        self.assertIn("A new list item", response.content.decode())
+        
+        self.assertEqual(Item.objects.all().count(), 1)
+        new_item = Item.objects.all()[0]
+        self.assertEqual(new_item.text, 'A new list item')
+        
+        
+        
+    def test_home_page_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+        
+        response = home_page(request)
+        
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
         
 class ItemModelTest(TestCase):
     
