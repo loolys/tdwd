2085c261cd5f5335190a585ea71e121cd61bfa2b
loolys <pierre.liljekvist@outlook.com>
2016-05-05 08:18:35 +0200

    Changed the any function in test to only an assertIn, added a string thingy to table in home.htlm added some functional tests for the table

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 
 from lists.views import home_page
@@ -15,8 +16,23 @@ class HomePageTests(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        
+        
         self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn(b'<title>To-Do lists</title>', response.content)
         self.assertTrue(response.content.strip().endswith(b'</html>'))
+        
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+        
+        response = home_page(request)
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': 'A new list item'},
+            request=request
+        )
+        self.assertEqual(response.content.decode(), expected_html)
+        self.assertIn("A new list item", response.content.decode())
