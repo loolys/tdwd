3c48b84d660340f610b4451c9dc8989232854c91
loolys <pierre.liljekvist@outlook.com>
2016-05-06 08:40:21 +0200

    functional tests now uses LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+from django.test import LiveServerTestCase
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -10,12 +11,13 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
     
+    
     def check_for_row_in_list_table(self,row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://127.0.0.1:8000')
+        self.browser.get(self.live_server_url)
         
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -32,7 +34,7 @@ class NewVisitorTest(unittest.TestCase):
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('User peacock feathers to make a fly')
+        inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         
         self.check_for_row_in_list_table('1: Buy peacock feathers')
@@ -40,5 +42,3 @@ class NewVisitorTest(unittest.TestCase):
         
         self.fail('Finish the test')
 
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
